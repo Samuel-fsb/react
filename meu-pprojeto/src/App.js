7ece26038b92890f5bbfe676ea5a5a4bce7fe3db
Samuel <samuel.fsbs@gmail.com>
2026-03-11 11:31:53 -0300

    Criando uma prop.

--- meu-pprojeto/src/App.js
+++ meu-pprojeto/src/App.js
@@ -1,28 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './componets/HelloWorld';
+import SayMyName from './componets/SayMyName'
 
 function App() {
+  return(
 
-  const Name = 'Matheus';
-
-  const NewName = Name.toUpperCase();
-
-  const Sum = (a, b) => {
-    return a + b;
-  };
-
-  const url = 'https://via.placeholder.com/150';
-
-  return (
-    <div className='App'>
-      <h1>Alterando o JSX</h1>
-      <p>Olá, {NewName}</p>
-      <p>Soma: {Sum(1, 4)}</p>
-      <img src={url} alt='Minha Imagem'></img>
-      <HelloWorld/>
-    </div>
   );
-}
+
+};
 
 export default App;
