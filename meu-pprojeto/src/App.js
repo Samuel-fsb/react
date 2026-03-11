fc76f67f5b4126ef7fc801dd6f1c9baf5ae20c9a
Samuel <samuel.fsbs@gmail.com>
2026-03-11 12:06:08 -0300

    Criando props e exportando.

--- meu-pprojeto/src/App.js
+++ meu-pprojeto/src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './componets/HelloWorld';
 import SayMyName from './componets/SayMyName'
+import Pessoa from './componets/Pessoa'
 
 function App() {
-  return(
 
+  const nome = 'Maira';
+
+  return(
+    <div className='App'>
+      <HelloWorld/>
+      <SayMyName nome="Julia"/>
+      <SayMyName nome="Samuel"/>
+      <SayMyName nome={nome}/>
+      <Pessoa nome="Rodrigo" 
+      idade="28" profissao="Programador" 
+      foto="https://via.placeholder.com/150"/>
+    </div>
   );
 
 };
