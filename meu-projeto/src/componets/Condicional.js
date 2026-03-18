a367903851ffc4fe557e7e1e04087932a0decd0a
Samuel <samuel.fsbs@gmail.com>
2026-03-18 09:50:35 -0300

    Criando formulário para a renderização de componentes.

--- meu-projeto/src/componets/Condicional.js
+++ meu-projeto/src/componets/Condicional.js
@@ -2,17 +2,26 @@ import { useState } from 'react'
 
 function Condicional(props){
 
+    const [email, setEmail] = useState()
+    const [userEmail, setUserEmail] = useState()
+
     function EnviarEmail(e){
         e.preventDefault()
-        console.log("Testando")
+        setUserEmail(email)
+        console.log(userEmail)
     };
 
     return(
         <div>
             <h2>Cadastre o seu e-mail</h2>
             <form>
-                <input type='email' placeholder='Digite o seu email...'></input>
+                <input type='email' placeholder='Digite o seu email...' onChange={(e) => setEmail(e.target.value)}></input>
                 <button type='submit' onClick={EnviarEmail}>Enviar Email</button>
+                {userEmail && (
+                    <div>
+                        <p>O e-mail do usuário é: {userEmail}</p>
+                    </div>
+                )}
             </form>
         </div>
     )
