e0dfca527cb184f94dbf6c18f59d6f65a2eaf503
Samuel <samuel.fsbs@gmail.com>
2026-03-20 14:58:29 -0300

    Criando o Route, switch e link dentro da rota.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,13 @@
 import {BrowerRouter as Router, Switch, Route, Link} from 'react-router-dom'
-
+import Home from './paginas/Home'
+import Empresa from './paginas/Empresa'
+import Contato from './paginas/Contato'
 
 function App() {
 
   return(
     <Router>
-      
+
       <ul>
         <li>
           <Link to="home">Home</Link>
@@ -24,6 +26,19 @@ function App() {
         </li>
       </ul>
 
+    <Routes>
+      <Route path='/Home'>
+          <Home></Home>
+      </Route>
+
+      
+      <Route path='/Empresa'>
+          <Empresa></Empresa>
+      </Route>
+      <Route path='/Contato'>
+          <Contato></Contato>
+      </Route>
+    </Routes>
     </Router>
   );
 
