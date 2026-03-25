69a502d8b4fa51f1880e6f68773344a06c339639
Samuel <samuel.fsbs@gmail.com>
2026-03-25 17:32:35 -0300

    Usando o ternário para mudar a cor do preço.

--- meu-projeto/src/componets/CartaoProduto.js
+++ meu-projeto/src/componets/CartaoProduto.js
@@ -3,7 +3,7 @@ function CartaoProduto({nome, preco, categoria, item=[], AplicarDesconto}){
     return(
         <div className="cartao">
             <h3>{nome}</h3>
-            <p>Preço: {preco}</p>
+            <p style={{color: preco > 50 ? 'red' : 'black'}}>Preço: {preco}</p>
             <span>Categoria: {categoria}</span>
             <ul>
                 {item.map((elemento, index) => <li key={index}>{elemento}</li>)}
