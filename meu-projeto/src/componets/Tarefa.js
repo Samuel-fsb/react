9d4f652369c770ca54678e40256145e87892c844
Samuel <samuel.fsbs@gmail.com>
2026-03-26 13:48:07 -0300

    Buscando dados com função assíncrona.

--- meu-projeto/src/componets/Tarefa.js
+++ meu-projeto/src/componets/Tarefa.js
@@ -1,8 +1,25 @@
-function Tarefa(){
+import { useState, useEffect } from "react";
+
+function Tarefa({ titulo, concluida}){
+
+    const [y] 
+
+
+    useEffect(() => {
+        const BuscarDados = async () => {
+            try {
+                const Dados = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5 (o _limit=5')
+            } catch (error) {
+                
+            }
+        }
+    });
+
+    
 
     return(
         <div>
-            
+
         </div>
     );
 
