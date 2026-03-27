87a0918d97e8b18219628a796cd7c50c4b16533e
Samuel <samuel.fsbs@gmail.com>
2026-03-27 09:37:12 -0300

    Estruturando o menu.

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,10 +1,27 @@
-
+import { browserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+    </Router>
+
+    <Container>
+      <Routes>
+
+        <Route exact path='/'>
+        <Home></Home>
+        </Route>
+
+        <Route exact path='/'>
+        <Home></Home>
+        </Route>
+      </Routes>
+    </Container>
+
   );
 }
 
