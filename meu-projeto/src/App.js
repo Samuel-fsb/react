e185c18111a2acaa0291e8f7e19eef4d3673a59e
Samuel <samuel.fsbs@gmail.com>
2026-03-28 16:25:11 -0300

    Baixando novos arquivos.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -14,7 +14,7 @@ function App() {
             } catch (error) {
                 console.log("Erro ao buscar os dados" + error);
             }
-        }
+        }k
 
         BuscarDados();
     }, []);
