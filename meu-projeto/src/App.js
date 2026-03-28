373ad25b9269a193bcd6d8b2240ba2e697c2ec04
Samuel <samuel.fsbs@gmail.com>
2026-03-28 09:13:15 -0300

    Renderizando os dados vindos da API em uma lista.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,18 +3,32 @@ import Tarefa from "./componets/Tarefa"
 
 function App() {
 
-  return(
-    <div>
-      <Tarefa />
-      useEffect(() => {
+  const [Lista, SetLista] = useState([]);
+
+  useEffect (() => {
         const BuscarDados = async () => {
             try {
-                const Dados = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+                const Dados = (await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5'))
+                const DadosFomatados = await Dados.json()
+                SetLista(DadosFomatados)
             } catch (error) {
-                
+                console.log("Erro ao buscar os dados" + error);
             }
         }
-    } []);
+
+        BuscarDados();
+    }, []);
+
+
+  return(
+    <div>
+    {Lista.map((item) => (
+    <Tarefa 
+      key={item.id}
+      titulo={item.title}
+      concluida={item.completed}
+    /> 
+    ))}
     </div>
   );
 
--- meu-projeto/src/componets/Tarefa.js
+++ meu-projeto/src/componets/Tarefa.js
@@ -4,12 +4,11 @@ function Tarefa({ titulo, concluida, AoRemover}){
 
     return(
         <div className="Item_Tarefa">
-            <sapn>concluida={concluida ? "✅" : "⏳"}</sapn>
-            <h1>texto={titulo}</h1>
+            <span>{concluida ? "✅" : "⏳"}</span>
+            <h1>{titulo}</h1>
             <button onClick={AoRemover}>Remover</button>
         </div>
     );
-
 
 };
 
