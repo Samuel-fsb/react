d7bbd2023c32b5ba482bd686658f8c205362dce8
Samuel <samuel.fsbs@gmail.com>
2026-04-07 16:13:53 -0300

    Estudando a renderização condicional.

--- curso-base-de-react/src/App.tsx
+++ curso-base-de-react/src/App.tsx
@@ -2,15 +2,24 @@ import { useState } from "react";
 
 export function App() {
 
-  const [ContarMaisUm, SetCount] = useState(0); 
-
+  const [Contar, SetCount] = useState(0); 
+  const [Hide, SetHide] = useState(false); 
   return (
     <div>
-      <button onClick={() => SetCount(ContarMaisUm + 1)}>
-     { ContarMaisUm }
-      
+
+      {Hide && <p>Teste 1</p>}
+      {!Hide && <p>Teste 2</p>} 
+
+      {Hide ? <p>Teste 1</p> : <p>Teste 2</p>}
+
+      <button onClick={() => SetCount(Contar+ 1)}>
+      {Contar}
       </button>
-    </div>
+
+      <button onClick={() => SetHide(!Hide)}>
+      Toggle
+      </button> 
+    </div> //Usei a exclamação para negar uma variável.
   )
 }
 
