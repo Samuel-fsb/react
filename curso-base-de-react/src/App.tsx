d342546e1eb0ed1d1c5a8a9352f83d2ec2154573
Samuel <samuel.fsbs@gmail.com>
2026-08-02 16:15:51 -0300

    Chamando o componente Person para o app.tsx

--- curso-base-de-react/src/App.tsx
+++ curso-base-de-react/src/App.tsx
@@ -1,4 +1,36 @@
 import { useState } from 'react';
+import './App.scss';
+import { Person } from './components/Person';
+
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+export const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
+
+export const App = () => (
+  <div className="App">
+    <Person person={misha}/>
+    <Person person={olya}/>
+    <Person person={alex}/>
+  </div>
+);
 
 export function App() {
 
--- curso-base-de-react/src/components/Person.jsx
+++ curso-base-de-react/src/components/Person.jsx
@@ -1,7 +1,8 @@
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
-    <p className="Person__age">I am {person.age}</p>
-    <p className="Person__parter">{person.parterName} is my wife</p>
+    {person.age && <p className="Person__age">I am {person.age}</p>}
+    {person.isMarried ? <p
+    className="Person__partner">{person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife' }</p> : <p className="Person__partner">I am not married</p>}
   </section>
 );
